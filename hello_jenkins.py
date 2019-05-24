5388cf1da07b8e21568948d20d3f724f7ae84134
YangShanL <393973270@qq.com>
2019-05-24 16:55:22 +0800

    test 3

--- hello_jenkins.py
+++ hello_jenkins.py
@@ -3,5 +3,5 @@ import time
 
 print('hello jenkins from python!')
 file = r'/opt/jenkins/script.log'
-with open(file,'a',encoding='utf8') as fi:
+with open(file,'a') as fi:
     fi.write(time.time())
